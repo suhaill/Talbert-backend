c4d7dad647e7acda45ce07aa9615a001d314afc7
suhail <mohammedsuhail@tatrasdata.com>
2017-10-03 18:32:56 +0530

    line item listing API

--- src/AppBundle/Controller/QuoteController.php
+++ src/AppBundle/Controller/QuoteController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use AppBundle\Entity\Quotes;
+use AppBundle\Entity\Veneer;
 use PDO;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -124,17 +125,18 @@ class QuoteController extends Controller
                 $arrApi['data']['id'] = $quoteData->getId();
                 $arrApi['data']['date'] = $quoteData->getEstimateDate();
                 //$arrApi['data']['estimatorId'] = $quoteData->getEstimatorId();
-                $arrApi['data']['controlnumber'] = $quoteData->getControlNumber();
+                $arrApi['data']['controlNumber'] = $quoteData->getControlNumber();
                 $arrApi['data']['version'] = $quoteData->getVersion();
-                $arrApi['data']['customer'] = $quoteData->getCustomerId();
+                $arrApi['data']['customer'] = $this->getCustomerNameById($quoteData->getCustomerId());
                 $arrApi['data']['referenceNumber'] = $quoteData->getRefNum();
-                $arrApi['data']['salesman'] = $quoteData->getSalesmanId();
+                $arrApi['data']['salesman'] = $this->getSalesmanNameById($quoteData->getSalesmanId());
                 $arrApi['data']['job'] = $quoteData->getJobName();
                 $arrApi['data']['term'] = $quoteData->getTermId();
-                $arrApi['data']['shipMethod'] = $quoteData->getShipMethdId();
-                $arrApi['data']['shipAdd'] = $quoteData->getShipAddId();
+                $arrApi['data']['shipMethod'] = $this->getShipMethodNamebyId($quoteData->getShipMethdId());
+                $arrApi['data']['shipAdd'] = $this->getShippingAddById($quoteData->getShipAddId());
                 $arrApi['data']['leadTime'] = $quoteData->getLeadTime();
                 $arrApi['data']['status'] = $quoteData->getStatus();
+                $arrApi['data']['lineitems'] = $this->getVeneerslistbyQuoteId($quoteId);
             }
         }
         catch(Exception $e) {
@@ -189,6 +191,18 @@ class QuoteController extends Controller
         }
     }
 
+    private function getSalesmanNameById($salesman_id) {
+        if (!empty($salesman_id)) {
+            $profileObj = $this->getDoctrine()
+                ->getRepository('AppBundle:Profile')
+                ->findOneBy(array('userId' => $salesman_id));
+            $customerName =  $profileObj->getFname();
+            if (!empty($customerName)) {
+                return $customerName;
+            }
+        }
+    }
+
     private function getEstimateDateFormate($date) {
         $dateArr =  explode('-', explode('T',$date)[0]);
         return $d = $dateArr[1].'/'.$dateArr[2].'/'.$dateArr[0];
@@ -196,5 +210,129 @@ class QuoteController extends Controller
 
     private function getQuoteDataById($qId) {
         return $this->getDoctrine()->getRepository('AppBundle:Quotes')->findOneById($qId);
+    }
+
+    private function getShippingAddById($add_id) {
+        $addArr = array();
+        $addData = $this->getDoctrine()->getRepository('AppBundle:Addresses')->findOneById($add_id);
+        if (!empty($addData)) {
+            $addArr['nickname'] = $addData->getNickname();
+            $addArr['street'] = $addData->getStreet();
+            $addArr['state'] = $this->getStateNamebyId($addData->getStateId());
+            $addArr['city'] = $addData->getCity();
+            $addArr['zip'] = $addData->getZip();
+            return $addArr;
+        }
+    }
+
+    private function getStateNamebyId($state_id) {
+        $stateRecord = $this->getDoctrine()->getRepository('AppBundle:State')->findOneById($state_id);
+        if (!empty($stateRecord)) {
+            return $stateRecord->getStateName();
+        }
+    }
+
+    private function getShipMethodNamebyId($hipmethod_id) {
+        $shippmethodRecord = $this->getDoctrine()->getRepository('AppBundle:ShippingMethods')->findOneById($hipmethod_id);
+        if (!empty($shippmethodRecord)) {
+            return $shippmethodRecord->getName();
+        }
+    }
+
+    private function getVeneerslistbyQuoteId($qId) {
+        $lineItem = array();
+        $plywoodRecords = $this->getDoctrine()->getRepository('AppBundle:Plywood')->findAll();
+        $veneerRecords = $this->getDoctrine()->getRepository('AppBundle:Veneer')->findBy(array('quoteId' => $qId));
+        $i=0;
+        if (!empty($plywoodRecords) || !empty($veneerRecords)) {
+            if (!empty($plywoodRecords)) {
+                foreach ($plywoodRecords as $p) {
+                    $lineItem[$i]['id'] = $p->getId();
+                    $lineItem[$i]['type'] = 'plywood';
+                    $lineItem[$i]['quantity'] = $p->getQuantity();
+                    $lineItem[$i]['species'] = $this->getSpeciesNameById($p->getSpeciesId());
+                    $lineItem[$i]['pattern'] = $this->getPatternNameById($p->getPatternId());
+                    $lineItem[$i]['grade'] = $this->getGradeNameById($p->getGradeId());
+                    $lineItem[$i]['back'] = $this->getBackNameById($p->getBackerId());
+                    $lineItem[$i]['thickness'] = $this->getThicknessNameById($p->getThicknessId());
+                    $lineItem[$i]['width'] = $p->getPlywoodWidth();
+                    $lineItem[$i]['length'] = $p->getPlywoodLength();
+                    $lineItem[$i]['core'] = $this->getCoreNameById($p->getCoreType());
+                    $lineItem[$i]['edge'] = $this->getEdgeNameById($p->getEdgeDetail());
+                    $lineItem[$i]['unitPrice'] = $p->getCost();
+                    $lineItem[$i]['totalPrice'] = $p->getCost();
+                    $i++;
+                }
+            }
+            if (!empty($veneerRecords)) {
+                foreach ($veneerRecords as $v) {
+                    $lineItem[$i]['id'] = $v->getId();
+                    $lineItem[$i]['type'] = 'veneer';
+                    $lineItem[$i]['quantity'] = $v->getQuantity();
+                    $lineItem[$i]['species'] = $this->getSpeciesNameById($v->getSpeciesId());
+                    $lineItem[$i]['pattern'] = $this->getPatternNameById($v->getPatternId());
+                    $lineItem[$i]['grade'] = $this->getGradeNameById($v->getGradeId());
+                    $lineItem[$i]['back'] = $this->getBackNameById($v->getBacker());
+                    $lineItem[$i]['thickness'] = $this->getThicknessNameById($v->getThicknessId());
+                    $lineItem[$i]['width'] = $v->getWidth();
+                    $lineItem[$i]['length'] = $v->getLength();
+                    $lineItem[$i]['core'] = $this->getCoreNameById($v->getCoreTypeId());
+                    $lineItem[$i]['edge'] = 'NA';
+                    $lineItem[$i]['unitPrice'] = 'NA';
+                    $lineItem[$i]['totalPrice'] = 'NA';
+                    $i++;
+                }
+            }
+            return $lineItem;
+        }
+    }
+
+    private function getSpeciesNameById($species_id) {
+        $productRecord = $this->getDoctrine()->getRepository('AppBundle:Product')->findOneById($species_id);
+        if (!empty($productRecord)) {
+            return $productRecord->getDescription();
+        }
+    }
+
+    private function getPatternNameById($pattern_id) {
+        $patternRecord = $this->getDoctrine()->getRepository('AppBundle:Pattern')->findOneById($pattern_id);
+        if (!empty($patternRecord)) {
+            return $patternRecord->getName();
+        }
+    }
+
+    private function getGradeNameById($gId) {
+        $gradeRecord = $this->getDoctrine()->getRepository('AppBundle:FaceGrade')->findOneById($gId);
+        if (!empty($gradeRecord)) {
+            return $gradeRecord->getName();
+        }
+    }
+
+    private function getBackNameById($bId) {
+        $backRecord = $this->getDoctrine()->getRepository('AppBundle:Backer')->findOneById($bId);
+        if (!empty($backRecord)) {
+            return $backRecord->getName();
+        }
+    }
+
+    private function getThicknessNameById($tId) {
+        $thicknessRecord = $this->getDoctrine()->getRepository('AppBundle:Thickness')->findOneById($tId);
+        if (!empty($thicknessRecord)) {
+            return $thicknessRecord->getName();
+        }
+    }
+
+    private function getCoreNameById($cId) {
+        $cTypeRecord = $this->getDoctrine()->getRepository('AppBundle:CoreType')->findOneById($cId);
+        if (!empty($cTypeRecord)) {
+            return $cTypeRecord->getName();
+        }
+    }
+
+    private function getEdgeNameById($eId) {
+        $eFinishRecord = $this->getDoctrine()->getRepository('AppBundle:EdgeFinish')->findOneById($eId);
+        if (!empty($eFinishRecord)) {
+            return $eFinishRecord->getName();
+        }
     }
 }
