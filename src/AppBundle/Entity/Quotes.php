a9199bca4956798d1ac6ca72a460ff8b5bf77080
suhail <mohammedsuhail@tatrasdata.com>
2018-02-20 15:23:23 +0530

    due-date

--- src/AppBundle/Entity/Quotes.php
+++ src/AppBundle/Entity/Quotes.php
@@ -52,6 +52,29 @@ class Quotes
     private $estimatedate;
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="delivery_date", type="string", length=30, nullable=true)
+     */
+    private $deliveryDate;
+
+    /**
+     * @return string
+     */
+    public function getDeliveryDate()
+    {
+        return $this->deliveryDate;
+    }
+
+    /**
+     * @param string $deliveryDate
+     */
+    public function setDeliveryDate($deliveryDate)
+    {
+        $this->deliveryDate = $deliveryDate;
+    }
+
+    /**
      * @var int
      *
      * @ORM\Column(name="estimator_id", type="integer")
