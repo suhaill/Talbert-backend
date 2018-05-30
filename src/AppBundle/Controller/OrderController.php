620a2ffa7083948cfacbbd935f0be2d3d44ff05a
mohit <mohit.k@tatrasdata.com>
2018-05-30 19:20:12 +0530

    bug fix - done

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -2928,7 +2928,7 @@ class OrderController extends Controller {
                         </tr>
                     </table></div></div></body></html>';
         $htmlArr['footer'] = '<!DOCTYPE html>
-        <html>
+        <html> 
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
